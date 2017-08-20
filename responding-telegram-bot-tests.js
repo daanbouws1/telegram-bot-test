23d27d4c066c8129d6f0c1a2222630550679097a
Daan Bouws <daan.bouws@hva.nl>
2017-08-20 18:15:58 +0200

    added responding tester + added hardcoded message_id for forwarding messages

--- responding-telegram-bot-tests.js
+++ responding-telegram-bot-tests.js
@@ -5,34 +5,42 @@ var telegram = require('telegram-bot-api');
 var assert = require('assert');
 var util = require('util');
 
-testTelegram();
+var api = new telegram({
+    token: '373890572:AAG5_y-nSgVM9EDM67ZqzGy_6c3l47Zf9kk',
+    updates: {
+        enabled:true
+    }
+});
 
-function testTelegram () {
+api.getMe().then(function(data) {
 
-    var api = new telegram({
-        token: '373890572:AAG5_y-nSgVM9EDM67ZqzGy_6c3l47Zf9kk',
-        updates: {
-            enabled: false
-        }
-    });
-
-    api.getMe().then(function (data) {
+        console.log(data);
         assert.equal(data.id, "373890572");
         assert.equal(data.first_name, 'asv_testbot');
         assert.equal(data.username, 'asv_testbot');
-    }).catch(function (err) {
+
+    })
+    .catch(function(err) {
         console.log(err);
     });
 
-    var chat_id = '1206425';
+api.on('message', function(message) {
 
-    api.sendMessage({chat_id: '1206425', text: "testmessage"}).then(function (data) {
-        assert.equal(data.text, "testmessage");
+    console.log(message.text);
+    console.log(message.message_id);
+    assert.equal(message.chat.type, "private");
+    assert.equal(message.from.first_name, 'Daan');
+    assert.equal(message.from.last_name, 'Bouws');
+
+    var chat_id = message.chat.id;
+
+    api.sendMessage({
+        chat_id: '1206425',
+        text: "testmessage"
+    }).then(function(data) {
+        assert.equal(data.text,"testmessage");
         assert.equal(data.chat.id, chat_id);
-        assert.equal(data.chat.type, "private");
-        assert.equal(data.chat.first_name, 'Daan');
-        assert.equal(data.chat.last_name, 'Bouws');
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
     });
 
@@ -40,43 +48,51 @@ function testTelegram () {
         chat_id: '1206425',
         caption: 'This is my test image',
         photo: 'croppedimage94.jpg'
-    }).then(function (data) {
+    }).then(function(data) {
         assert.equal(data.photo.length, 4);
         assert.equal(data.caption, 'This is my test image');
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
     });
 
-    api.forwardMessage({chat_id: '1206425', from_chat_id: '1206425',
-        message_id: '264'
+    api.forwardMessage({
+        chat_id: message.chat.id,
+        from_chat_id: message.chat.id,
+        message_id: message.message_id
     }).then(function(message) {
-        assert.equal(message.forward_from.first_name,"Daan");
-        assert.equal(message.forward_from.last_name, "Bouws");
+        assert.equal(message.forward_from.first_name,"Daan");
+        assert.equal(message.forward_from.last_name, "Bouws");
     }).catch(function(err) {
-        console.log(err);
+        console.log(err);
     });
 
-    api.getUserProfilePhotos({user_id: "1206425", limit: 1}).then(function (data) {
+    api.getUserProfilePhotos({
+        user_id: "1206425",
+        limit: 1
+    }).then(function(data){
         assert.equal(data.total_count, 2);
         assert.equal(data.photos[0].length, 3);
-    }).catch(function (err) {
+    }).catch(function(err){
         console.log(err);
     });
 
-    api.sendSticker({chat_id: '1206425', sticker: 'sticker.webp'}).then(function (data) {
+    api.sendSticker({
+        chat_id: '1206425',
+        sticker: 'sticker.webp'
+    }).then(function(data){
         assert.equal(data.sticker.thumb.width, 64);
-    }).catch(function (err) {
+    }).catch(function(err){
         console.log(err);
     });
 
     api.sendAudio({
         chat_id: '1206425',
         audio: 'bell-ringing-01.mp3'
-    }).then(function (data) {
+    }).then(function(data) {
         assert.equal(data.audio.duration, 17);
         assert.equal(data.audio.mime_type, "audio/mpeg");
         assert.equal(data.audio.file_size, 686758);
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log(err);
     });
 
@@ -84,12 +100,11 @@ function testTelegram () {
         chat_id: '1206425',
         latitude: 50.101,
         longitude: 52.101
-    }).then(function (data) {
+    }).then(function(data){
         assert.equal(data.location.longitude, 52.100998);
         assert.equal(data.location.latitude, 50.100992);
-    }).catch(function (err) {
+    }).catch(function(err){
         console.log(err);
     });
-}
 
-
+});
